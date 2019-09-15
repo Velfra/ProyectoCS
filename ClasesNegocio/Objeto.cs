cb1c20a2d05a26117d550c8f3177a901f836416e
Mathias Diaz <mathyrock62@gmail.com>
2019-09-15 18:11:24 -0400

    Clases Producto,Objeto,Producto_Limpieza agregadas

---
+++ ClasesNegocio/Objeto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesNegocio
+{
+    class Objeto
+    {
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public DateTime Fecha_Vencimiento { get; set; }
+    }
+}
