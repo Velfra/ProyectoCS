c9994d53e8822e568ec30a6fd9db4dbbfadfcf28
Mathias Diaz <mathyrock62@gmail.com>
2019-10-20 14:50:48 -0300

    Formulario Proveedor - Actualizado

--- ControlStock/frmProveedor.Designer.cs
+++ ControlStock/frmProveedor.Designer.cs
@@ -28,90 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblProveedor = new System.Windows.Forms.Label();
-            this.txtNombreProveedor = new System.Windows.Forms.TextBox();
-            this.txtDireccion = new System.Windows.Forms.TextBox();
-            this.lblDireccion = new System.Windows.Forms.Label();
-            this.txtTelefono = new System.Windows.Forms.TextBox();
-            this.lblTelefono = new System.Windows.Forms.Label();
-            this.txtMail = new System.Windows.Forms.TextBox();
-            this.lblMail = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btnCancelar = new System.Windows.Forms.Button();
-            this.btnLimpiar = new System.Windows.Forms.Button();
-            this.btnGuardar = new System.Windows.Forms.Button();
+            this.tbcPrincipal = new System.Windows.Forms.TabControl();
+            this.tbpABM = new System.Windows.Forms.TabPage();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.btnEliminar = new System.Windows.Forms.Button();
             this.btnModificar = new System.Windows.Forms.Button();
             this.btnAgregar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.lblMail = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.txtNombreProveedor = new System.Windows.Forms.TextBox();
+            this.lblProveedor = new System.Windows.Forms.Label();
+            this.tbpConsulta = new System.Windows.Forms.TabPage();
             this.lstProveedor = new System.Windows.Forms.ListBox();
-            this.groupBox1.SuspendLayout();
+            this.tbcPrincipal.SuspendLayout();
+            this.tbpABM.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tbpConsulta.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lblProveedor
+            // tbcPrincipal
             // 
-            this.lblProveedor.AutoSize = true;
-            this.lblProveedor.Location = new System.Drawing.Point(18, 60);
-            this.lblProveedor.Name = "lblProveedor";
-            this.lblProveedor.Size = new System.Drawing.Size(58, 17);
-            this.lblProveedor.TabIndex = 0;
-            this.lblProveedor.Text = "Nombre";
+            this.tbcPrincipal.Controls.Add(this.tbpABM);
+            this.tbcPrincipal.Controls.Add(this.tbpConsulta);
+            this.tbcPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbcPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.tbcPrincipal.Name = "tbcPrincipal";
+            this.tbcPrincipal.SelectedIndex = 0;
+            this.tbcPrincipal.Size = new System.Drawing.Size(573, 513);
+            this.tbcPrincipal.TabIndex = 0;
             // 
-            // txtNombreProveedor
+            // tbpABM
             // 
-            this.txtNombreProveedor.Location = new System.Drawing.Point(96, 60);
-            this.txtNombreProveedor.Name = "txtNombreProveedor";
-            this.txtNombreProveedor.Size = new System.Drawing.Size(194, 22);
-            this.txtNombreProveedor.TabIndex = 1;
+            this.tbpABM.Controls.Add(this.groupBox2);
+            this.tbpABM.Controls.Add(this.groupBox1);
+            this.tbpABM.Location = new System.Drawing.Point(4, 25);
+            this.tbpABM.Name = "tbpABM";
+            this.tbpABM.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpABM.Size = new System.Drawing.Size(565, 484);
+            this.tbpABM.TabIndex = 0;
+            this.tbpABM.Text = "ABM";
+            this.tbpABM.UseVisualStyleBackColor = true;
             // 
-            // txtDireccion
+            // groupBox2
             // 
-            this.txtDireccion.Location = new System.Drawing.Point(96, 107);
-            this.txtDireccion.Name = "txtDireccion";
-            this.txtDireccion.Size = new System.Drawing.Size(194, 22);
-            this.txtDireccion.TabIndex = 3;
+            this.groupBox2.Controls.Add(this.btnEliminar);
+            this.groupBox2.Controls.Add(this.btnModificar);
+            this.groupBox2.Controls.Add(this.btnAgregar);
+            this.groupBox2.Location = new System.Drawing.Point(21, 317);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(506, 127);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Procesos";
             // 
-            // lblDireccion
+            // btnEliminar
             // 
-            this.lblDireccion.AutoSize = true;
-            this.lblDireccion.Location = new System.Drawing.Point(18, 107);
-            this.lblDireccion.Name = "lblDireccion";
-            this.lblDireccion.Size = new System.Drawing.Size(67, 17);
-            this.lblDireccion.TabIndex = 2;
-            this.lblDireccion.Text = "Direccion";
+            this.btnEliminar.Location = new System.Drawing.Point(199, 52);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(81, 35);
+            this.btnEliminar.TabIndex = 13;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
-            // txtTelefono
+            // btnModificar
             // 
-            this.txtTelefono.Location = new System.Drawing.Point(96, 154);
-            this.txtTelefono.Name = "txtTelefono";
-            this.txtTelefono.Size = new System.Drawing.Size(194, 22);
-            this.txtTelefono.TabIndex = 5;
+            this.btnModificar.Location = new System.Drawing.Point(320, 52);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(81, 35);
+            this.btnModificar.TabIndex = 12;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
             // 
-            // lblTelefono
+            // btnAgregar
             // 
-            this.lblTelefono.AutoSize = true;
-            this.lblTelefono.Location = new System.Drawing.Point(18, 154);
-            this.lblTelefono.Name = "lblTelefono";
-            this.lblTelefono.Size = new System.Drawing.Size(64, 17);
-            this.lblTelefono.TabIndex = 4;
-            this.lblTelefono.Text = "Telefono";
-            // 
-            // txtMail
-            // 
-            this.txtMail.Location = new System.Drawing.Point(96, 203);
-            this.txtMail.Name = "txtMail";
-            this.txtMail.Size = new System.Drawing.Size(194, 22);
-            this.txtMail.TabIndex = 7;
-            // 
-            // lblMail
-            // 
-            this.lblMail.AutoSize = true;
-            this.lblMail.Location = new System.Drawing.Point(18, 203);
-            this.lblMail.Name = "lblMail";
-            this.lblMail.Size = new System.Drawing.Size(47, 17);
-            this.lblMail.TabIndex = 6;
-            this.lblMail.Text = "E-mail";
+            this.btnAgregar.Location = new System.Drawing.Point(84, 52);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(81, 35);
+            this.btnAgregar.TabIndex = 11;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // groupBox1
             // 
@@ -126,10 +133,10 @@
             this.groupBox1.Controls.Add(this.lblDireccion);
             this.groupBox1.Controls.Add(this.txtNombreProveedor);
             this.groupBox1.Controls.Add(this.lblProveedor);
-            this.groupBox1.Location = new System.Drawing.Point(22, 25);
+            this.groupBox1.Location = new System.Drawing.Point(21, 20);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(481, 272);
-            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabIndex = 13;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Datos Proveedor";
             // 
@@ -163,96 +170,134 @@
             this.btnGuardar.UseVisualStyleBackColor = true;
             this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
-            // groupBox2
+            // txtMail
             // 
-            this.groupBox2.Controls.Add(this.btnEliminar);
-            this.groupBox2.Controls.Add(this.btnModificar);
-            this.groupBox2.Controls.Add(this.btnAgregar);
-            this.groupBox2.Location = new System.Drawing.Point(22, 322);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(169, 276);
-            this.groupBox2.TabIndex = 9;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Procesos";
+            this.txtMail.Location = new System.Drawing.Point(96, 203);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(194, 22);
+            this.txtMail.TabIndex = 7;
             // 
-            // btnEliminar
+            // lblMail
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(44, 134);
-            this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(81, 35);
-            this.btnEliminar.TabIndex = 13;
-            this.btnEliminar.Text = "Eliminar";
-            this.btnEliminar.UseVisualStyleBackColor = true;
-            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            this.lblMail.AutoSize = true;
+            this.lblMail.Location = new System.Drawing.Point(18, 203);
+            this.lblMail.Name = "lblMail";
+            this.lblMail.Size = new System.Drawing.Size(47, 17);
+            this.lblMail.TabIndex = 6;
+            this.lblMail.Text = "E-mail";
             // 
-            // btnModificar
+            // txtTelefono
             // 
-            this.btnModificar.Location = new System.Drawing.Point(44, 203);
-            this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(81, 35);
-            this.btnModificar.TabIndex = 12;
-            this.btnModificar.Text = "Modificar";
-            this.btnModificar.UseVisualStyleBackColor = true;
-            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            this.txtTelefono.Location = new System.Drawing.Point(96, 154);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(194, 22);
+            this.txtTelefono.TabIndex = 5;
             // 
-            // btnAgregar
+            // lblTelefono
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(44, 65);
-            this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(81, 35);
-            this.btnAgregar.TabIndex = 11;
-            this.btnAgregar.Text = "Agregar";
-            this.btnAgregar.UseVisualStyleBackColor = true;
-            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(18, 154);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(64, 17);
+            this.lblTelefono.TabIndex = 4;
+            this.lblTelefono.Text = "Telefono";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(96, 107);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(194, 22);
+            this.txtDireccion.TabIndex = 3;
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(18, 107);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(67, 17);
+            this.lblDireccion.TabIndex = 2;
+            this.lblDireccion.Text = "Direccion";
+            // 
+            // txtNombreProveedor
+            // 
+            this.txtNombreProveedor.Location = new System.Drawing.Point(96, 60);
+            this.txtNombreProveedor.Name = "txtNombreProveedor";
+            this.txtNombreProveedor.Size = new System.Drawing.Size(194, 22);
+            this.txtNombreProveedor.TabIndex = 1;
+            // 
+            // lblProveedor
+            // 
+            this.lblProveedor.AutoSize = true;
+            this.lblProveedor.Location = new System.Drawing.Point(18, 60);
+            this.lblProveedor.Name = "lblProveedor";
+            this.lblProveedor.Size = new System.Drawing.Size(58, 17);
+            this.lblProveedor.TabIndex = 0;
+            this.lblProveedor.Text = "Nombre";
+            // 
+            // tbpConsulta
+            // 
+            this.tbpConsulta.Controls.Add(this.lstProveedor);
+            this.tbpConsulta.Location = new System.Drawing.Point(4, 25);
+            this.tbpConsulta.Name = "tbpConsulta";
+            this.tbpConsulta.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpConsulta.Size = new System.Drawing.Size(565, 484);
+            this.tbpConsulta.TabIndex = 1;
+            this.tbpConsulta.Text = "Consulta";
+            this.tbpConsulta.UseVisualStyleBackColor = true;
             // 
             // lstProveedor
             // 
+            this.lstProveedor.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lstProveedor.FormattingEnabled = true;
             this.lstProveedor.ItemHeight = 16;
-            this.lstProveedor.Location = new System.Drawing.Point(224, 322);
+            this.lstProveedor.Location = new System.Drawing.Point(3, 3);
             this.lstProveedor.Margin = new System.Windows.Forms.Padding(4);
             this.lstProveedor.Name = "lstProveedor";
-            this.lstProveedor.Size = new System.Drawing.Size(279, 276);
-            this.lstProveedor.TabIndex = 12;
-            this.lstProveedor.Click += new System.EventHandler(this.lstProveedor_Click);
+            this.lstProveedor.Size = new System.Drawing.Size(559, 478);
+            this.lstProveedor.TabIndex = 16;
+            this.lstProveedor.DoubleClick += new System.EventHandler(this.lstProveedor_DoubleClick);
             // 
             // frmProveedor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(529, 638);
-            this.Controls.Add(this.lstProveedor);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(573, 513);
+            this.Controls.Add(this.tbcPrincipal);
             this.Name = "frmProveedor";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Proveedor";
             this.Load += new System.EventHandler(this.frmProveedor_Load);
+            this.tbcPrincipal.ResumeLayout(false);
+            this.tbpABM.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
+            this.tbpConsulta.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label lblProveedor;
-        private System.Windows.Forms.TextBox txtNombreProveedor;
-        private System.Windows.Forms.TextBox txtDireccion;
-        private System.Windows.Forms.Label lblDireccion;
-        private System.Windows.Forms.TextBox txtTelefono;
-        private System.Windows.Forms.Label lblTelefono;
-        private System.Windows.Forms.TextBox txtMail;
-        private System.Windows.Forms.Label lblMail;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button btnCancelar;
-        private System.Windows.Forms.Button btnLimpiar;
-        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TabControl tbcPrincipal;
+        private System.Windows.Forms.TabPage tbpABM;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button btnEliminar;
         private System.Windows.Forms.Button btnModificar;
         private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label lblMail;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.TextBox txtNombreProveedor;
+        private System.Windows.Forms.Label lblProveedor;
+        private System.Windows.Forms.TabPage tbpConsulta;
         private System.Windows.Forms.ListBox lstProveedor;
     }
 }
