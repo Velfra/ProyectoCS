29548a5fc003fdd80097cf67672f218cc7451259
Mathias Diaz <mathyrock62@gmail.com>
2019-10-20 10:11:38 -0300

    Se actualizo la clase proveedor

--- ClasesNegocio/Proveedor.cs
+++ ClasesNegocio/Proveedor.cs
@@ -9,9 +9,38 @@ namespace ClasesNegocio
     public class Proveedor
     {
         public string NombreProveedor { get; set; }
-        public DateTime FechaEntrega { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
 
+        public override string ToString()
+        {
+            return NombreProveedor;
+        }
 
-        public Proveedor () { }
+        public static List<Proveedor> listaProveedores = new List<Proveedor>();
+
+        public static List<Proveedor> ObtenerProveedores()
+        {
+            return listaProveedores;
+        }
+
+      
+
+        public static void AgregarProveedor(Proveedor p)
+        {
+            listaProveedores.Add(p);
+        }
+
+        public static void EliminarProveedor(Proveedor p)
+        {
+            listaProveedores.Remove(p);
+        }
+
+        public static void EditarProveedor(Proveedor p, int indice)
+        {
+
+            Proveedor.listaProveedores[indice] = p;
+        }
     }
 }
