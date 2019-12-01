552caf71240b6f4dc955256b16e98da087523498
Mathias Diaz <mathyrock62@gmail.com>
2019-11-30 23:26:56 -0300

    Creacion de login de usuario

---
+++ ControlStock/frm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ControlStock
+{
+    partial class frmConfiguracion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmConfiguracion));
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.txtBaseDatos = new System.Windows.Forms.TextBox();
+            this.txtServidor = new System.Windows.Forms.TextBox();
+            this.lblBaseDatos = new System.Windows.Forms.Label();
+            this.lblServidor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancelar.Location = new System.Drawing.Point(188, 104);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(116, 38);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "&Cancelar";
+            this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Image = ((System.Drawing.Image)(resources.GetObject("btnAceptar.Image")));
+            this.btnAceptar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAceptar.Location = new System.Drawing.Point(28, 104);
+            this.btnAceptar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(116, 38);
+            this.btnAceptar.TabIndex = 10;
+            this.btnAceptar.Text = "&Aceptar";
+            this.btnAceptar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // txtBaseDatos
+            // 
+            this.txtBaseDatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBaseDatos.Location = new System.Drawing.Point(126, 63);
+            this.txtBaseDatos.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBaseDatos.Name = "txtBaseDatos";
+            this.txtBaseDatos.Size = new System.Drawing.Size(191, 23);
+            this.txtBaseDatos.TabIndex = 9;
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtServidor.Location = new System.Drawing.Point(126, 23);
+            this.txtServidor.Margin = new System.Windows.Forms.Padding(4);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.Size = new System.Drawing.Size(191, 23);
+            this.txtServidor.TabIndex = 8;
+            // 
+            // lblBaseDatos
+            // 
+            this.lblBaseDatos.AutoSize = true;
+            this.lblBaseDatos.Location = new System.Drawing.Point(13, 67);
+            this.lblBaseDatos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblBaseDatos.Name = "lblBaseDatos";
+            this.lblBaseDatos.Size = new System.Drawing.Size(101, 17);
+            this.lblBaseDatos.TabIndex = 7;
+            this.lblBaseDatos.Text = "Base de Datos";
+            // 
+            // lblServidor
+            // 
+            this.lblServidor.AutoSize = true;
+            this.lblServidor.Location = new System.Drawing.Point(13, 26);
+            this.lblServidor.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblServidor.Name = "lblServidor";
+            this.lblServidor.Size = new System.Drawing.Size(61, 17);
+            this.lblServidor.TabIndex = 6;
+            this.lblServidor.Text = "Servidor";
+            // 
+            // frmConfiguracion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 170);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtBaseDatos);
+            this.Controls.Add(this.txtServidor);
+            this.Controls.Add(this.lblBaseDatos);
+            this.Controls.Add(this.lblServidor);
+            this.Name = "frmConfiguracion";
+            this.Text = "Configuracion del Sistema";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnCancelar;
+        internal System.Windows.Forms.Button btnAceptar;
+        internal System.Windows.Forms.TextBox txtBaseDatos;
+        internal System.Windows.Forms.TextBox txtServidor;
+        internal System.Windows.Forms.Label lblBaseDatos;
+        internal System.Windows.Forms.Label lblServidor;
+    }
+}
---
+++ ControlStock/frm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.Devices;
+
+namespace ControlStock
+{
+    public partial class frmConfiguracion : Form
+    {
+        public frmConfiguracion()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
